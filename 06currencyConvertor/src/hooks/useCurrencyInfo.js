14e6d92e4420f5bb0ad7875df8d1ed3dce717922
pabhinav689@gmail.com <pabhinav689@gmail.com>
2024-08-07 15:27:47 +0530

    first commit

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react"
+
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://v6.exchangerate-api.com/v6/2dc0b2955ae90111c957e5f2/latest/USD`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
+}
+
+export default useCurrencyInfo;
